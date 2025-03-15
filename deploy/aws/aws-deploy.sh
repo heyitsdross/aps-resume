b9a8eb238c29eb69d15ecabc821af5a40f0cd146
Austin Schaefer <schaefer.austin.p@inbox.eu>
2025-03-15 11:43:47 +0000

    Use latest git tag instead of env var in AWS deploy.

--- deploy/aws/aws-deploy.sh
+++ deploy/aws/aws-deploy.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
- if [ -z "${APS_RESUME_VERSION}" ]; then
+# Attempt safe checkout of master branch
+git stash && git checkout master
+
+# Get latest release tag
+image_version=$(git describe --tags --abbrev=0)
+
+ if [ -z "$image_version" ]; then
    echo "Missing release version env variable."
    exit 1
  fi
 
-docker build . -t aps-resume:"${APS_RESUME_VERSION}" \
-  --build-arg user="${USER}" \
-  --no-cache
+docker build . -t aps-resume:"$image_version" --no-cache
 
-echo "Uploading image version ${APS_RESUME_VERSION}."
+echo "Uploading image version $image_version."
 
 aws lightsail push-container-image \
- --image aps-resume:"${APS_RESUME_VERSION}" \
+ --image aps-resume:"$image_version" \
  --label resume-website-container \
  --service-name resume-container-service
 
